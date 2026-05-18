517eabb491e782ac612365c6987ff2ce2d9b12fd
root <root@vmi3304846.contaboserver.net>
2026-05-18 09:00:26 +0200

    fix(e2e): reorder env exports in start-server.sh, use relative cd

--- start-server.sh
+++ start-server.sh
@@ -2,7 +2,8 @@
 set -a
 source /tmp/shop-original/.env
 set +a
+export PLAYWRIGHT_TEST_BASE_URL=http://localhost:3000
 export NODE_ENV=production
 export MOCKS=true
-export PLAYWRIGHT_TEST_BASE_URL=http://localhost:3000
-exec node /tmp/shop-original/server-build/index.js
+cd /tmp/shop-original
+exec node ./server-build/index.js
